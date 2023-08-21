8bd9ce65b4da815fa38a86e3084c91368e9efd1b
Peter Farber <farberpete@gmail.com>
2023-08-21 13:49:37 -0400

    Refactored ArweaveWebWallet.ts and Added specify disconnect functionality.

--- projects/arweave-wallet-kit-ng/src/lib/strategy/strategies/ArweaveWebWallet.ts
+++ projects/arweave-wallet-kit-ng/src/lib/strategy/strategies/ArweaveWebWallet.ts
@@ -1,18 +1,18 @@
-import type { AppInfo, GatewayConfig, PermissionType } from "arconnect";
-import { ArweaveWebWallet } from "arweave-wallet-connector";
-import BrowserWalletStrategy from "./BrowserWallet";
-import Strategy from "../Strategy";
+import type { AppInfo, GatewayConfig, PermissionType } from 'arconnect';
+import { ArweaveWebWallet } from 'arweave-wallet-connector';
+import BrowserWalletStrategy from './BrowserWallet';
+import Strategy from '../Strategy';
 
 export default class ArweaveWebWalletStrategy
   extends BrowserWalletStrategy
   implements Strategy
 {
-  public override id: string = "webwallet";
-  public override name: string = "Arweave.app";
-  public override description: string = "Web based wallet software";
-  public override theme: string = "24, 24, 24";
-  public override logo: string = "qVms-k8Ox-eKFJN5QFvrPQvT9ryqQXaFcYbr-fJbgLY";
-  public url: string = "https://arweave.app";
+  public override id: string = 'webwallet';
+  public override name: string = 'Arweave.app';
+  public override description: string = 'Web based wallet software';
+  public override theme: string = '24, 24, 24';
+  public override logo: string = 'qVms-k8Ox-eKFJN5QFvrPQvT9ryqQXaFcYbr-fJbgLY';
+  public url: string = 'https://arweave.app';
 
   #instance = new ArweaveWebWallet();
 
@@ -20,13 +20,18 @@ export default class ArweaveWebWalletStrategy
     super();
   }
 
-  public override async isAvailable() {
+  public override async isAvailable(): Promise<boolean> {
     return true;
   }
 
-  public async resumeSession() {
-    this.#instance.setUrl("arweave.app");
+  public async resumeSession(): Promise<void> {
+    this.#instance.setUrl('arweave.app');
     await this.#instance.connect();
+  }
+
+  public override async disconnect(): Promise<void> {
+    this.#instance.setUrl('arweave.app');
+    await this.#instance.disconnect();
   }
 
   public override async connect(
@@ -36,7 +41,7 @@ export default class ArweaveWebWalletStrategy
   ): Promise<void> {
     if (gateway) {
       console.warn(
-        "[Arweave Wallet Kit] Arweave.app does not support custom gateway connection yet."
+        '[Arweave Wallet Kit] Arweave.app does not support custom gateway connection yet.'
       );
     }
 
@@ -46,12 +51,12 @@ export default class ArweaveWebWalletStrategy
   }
 
   public override addAddressEvent(listener: (address: string) => void) {
-    this.#instance.on("connect", listener);
+    this.#instance.on('connect', listener);
 
     return listener as any;
   }
 
   public override removeAddressEvent(listener: any) {
-    this.#instance.off("connect", listener);
+    this.#instance.off('connect', listener);
   }
 }
